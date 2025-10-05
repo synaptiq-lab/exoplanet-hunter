8c96d031d49e88b26e4720a52645d921c452ba4f
opixelum <48217573+opixelum@users.noreply.github.com>
2025-10-05 03:44:05 +0200

    feat(exominer): format inputs for model

--- backend/exominer_helper.py
+++ backend/exominer_helper.py
@@ -2,6 +2,14 @@ from astroquery.mast import Observations
 import polars as pl
 
 
+def format_inputs(df: pl.DataFrame) -> pl.DataFrame:
+    return (
+        df.explode("sectors")
+        .with_columns(pl.format("{}-{}", pl.col("sectors"), pl.col("sectors")).alias("sector_run"))
+        .select(["tic_id", "sector_run"])
+    )
+
+
 def get_sectors_from_tic(tic: int) -> list[int]:
     try:
         obs = Observations.query_criteria(
@@ -29,12 +37,14 @@ def build_results_table(path_to_result_csv: str) -> pl.DataFrame:
             .otherwise(pl.lit("Confirmed"))
             .alias("result")
         )
-        .rename({"target_id": "tic"})
-        .select(["tic", "result", "score"])
+        .rename({"target_id": "tic_id"})
+        .select(["tic_id", "result", "score"])
         .collect()
     )
 
 
 if __name__ == "__main__":
     # print(build_results_table("../data/predictions_outputs.csv"))
-    print(get_sectors_from_tic(51432))
+    # print(get_sectors_from_tic(51432))
+    unformatted_inputs_df = pl.scan_parquet("../data/tics-with-sectors.parquet").head(3).collect()
+    print(format_inputs(unformatted_inputs_df))
